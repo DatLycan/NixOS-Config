d761e88d159a669c0506bb09d9e111b3f803a2c3
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-15 22:07:01 +0100

    Moved configuration.nix to common

--- common/system.nix
+++ common/system.nix
@@ -1,8 +1,40 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports =
     [ 
       ../modules/system/default-user.nix
     ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+     neovim 
+     wget
+     git
+     nix-ld
+     nil
+  ];
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      PermitRootLogin = "no";
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
