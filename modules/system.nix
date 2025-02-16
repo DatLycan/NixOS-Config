b9f723b5dcb9cbc10e61d1457cb4092a9e57e09c
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-16 11:51:19 +0100

    Code cleanup

--- modules/system.nix
+++ modules/system.nix
@@ -8,9 +8,11 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
 
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -19,13 +21,13 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   environment.systemPackages = with pkgs; [
-    nh
-    neovim 
-    wget
-    git
-    nix-ld
-    nil
-    hyprland
+    pkgs.neovim 
+    pkgs.curl
+    pkgs.wget
+    pkgs.git
+    pkgs.nix-ld
+    pkgs.nil
+    pkgs.hyprland
   ];
 
   services.openssh = {
