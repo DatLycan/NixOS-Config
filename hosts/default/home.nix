c10e1b13a33f06c960edc840cf17cc91fe7a918e
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-16 01:03:33 +0100

    Added .nix files

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,4 +6,20 @@
   home.username = "datlycan";
   home.homeDirectory = "/home/datlycan";
 
+  home.stateVersion = "24.11";
+
+  home.packages = [
+
+  ];
+
+  home.file = {
+
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
