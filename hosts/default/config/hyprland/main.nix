12cf1324cc006c594bbe8f152d91c08e8eb8a42e
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-16 19:33:14 +0100

    Edited hyprland

--- hosts/default/config/hyprland/main.nix
+++ hosts/default/config/hyprland/main.nix
@@ -1,38 +1,48 @@
+
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = true;
-settings = {
+    settings = {
       monitor = ",preferred, 1080x720@60 ,auto";
 
-      env = {
-        XCURSOR_SIZE = "14";
-        HYPRCURSOR_SIZE = "24";
-      };
+      "$mainMod" = "SUPER";
+
+      env = [
+        # Hint Electron apps to use Wayland
+        "NIXOS_OZONE_WL,1"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "QT_QPA_PLATFORM,wayland"
+        "XDG_SCREENSHOTS_DIR,$HOME/screens"
+      ];
+
+      exec-once = [
+        "waybar"
+      ];
 
       general = {
         gaps_in = 5;
         gaps_out = 20;
+        
         border_size = 2;
-        col = {
-          active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          inactive_border = "rgba(595959aa)";
-        };
-        resize_on_border = false;
+
+        "col.active_border" = "rgba(d65d0eff) rgba(98971aff) 45deg";
+        "col.inactive_border" = "rgba(3c3836ff)";
+
+        resize_on_border = true;
         allow_tearing = false;
         layout = "dwindle";
       };
 
       decoration = {
-        rounding = 0;
+        rounding = 5;
 
         active_opacity = 1.0;
         inactive_opacity = 1.0;
-
         shadow = {
           enabled = false;
         };
-
         blur = {
           enabled = false;
         };
@@ -49,38 +59,27 @@ settings = {
 
       master = {
         new_status = "master";
+        new_on_top = true;
+        mfact = 0.5;
       };
 
       misc = {
-        force_default_wallpaper = 0;
+        force_default_wallpaper = -1;
         disable_hyprland_logo = true;
       };
 
       input = {
         kb_layout = "us";
-        follow_mouse = 1;
-        sensitivity = 0;
-        touchpad = {
-          natural_scroll = false;
-        };
+        kb_options = "grp:caps_toggle";
       };
 
       gestures = {
         workspace_swipe = false;
       };
 
-      device = {
-        name = "epic-mouse-v1";
-        sensitivity = -0.5;
-      };
-
       windowrules = [
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
-
-      exec-once = [
-        "waybar"
       ];
     };
   };
