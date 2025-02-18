94159aca37ed3fd6380d1af4d9c79a148308474a
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-18 21:24:57 +0100

    Changed file name

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -5,25 +5,16 @@
     [ 
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
-      
-      builtins.mapAttrsToList (name: value: value) (builtins.readDir ../../modules);
-    ];
+    ] ++ map (name: ../../modules + ("/" + name)) (builtins.attrNames (builtins.readDir ../../modules));
 
   networking.hostName = common.default.hostName;
 
-  home-manager = {
-    extraSpecialArgs = { 
-      inherit inputs; 
-    };
-
-    users = { 
-        "${config.default-user.userName}" = import ./home.nix; 
-    };
-  };
+  home-manager.extraSpecialArgs = { inherit inputs; };
+  home-manager.users."${config.default-user.userName}" = import ./home.nix;
 
   default-user.enable = true;
   default-editor.enable = true;
   default-style.enable = true;
   
-  programs.nix-ld.enable = true; #TODO: Write a module for this vscode server as user module
+  programs.nix-ld.enable = true; # TODO: Write a module for this vscode server as user module
 }
