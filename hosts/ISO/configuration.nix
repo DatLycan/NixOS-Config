b169d9e8010c16676813b86f70c3929e6f25fb75
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-19 18:38:59 +0100

    Fixed configuration.nix

--- hosts/ISO/configuration.nix
+++ hosts/ISO/configuration.nix
@@ -1,10 +1,9 @@
-{ config, pkgs, system, modulesPath, ... }:
+{ config, pkgs, modulesPath, ... }: {
 
-{
-  imports =
-  [ 
+  imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
-  ];
+  ] ++ map (name: ../../modules/system + ("/" + name)) (builtins.attrNames (builtins.readDir ../../modules/system));
 
+  nixpkgs.hostPlatform = "x86_64-linux";
   default-user.enable = true;
 }
