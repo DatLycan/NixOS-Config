f9a5bfa25bd4b20749b8b630efdc4a2359561e37
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-20 02:31:42 +0100

    Added theming

--- modules/system/default-editor/nvf-settings.nix
+++ modules/system/default-editor/nvf-settings.nix
@@ -4,11 +4,11 @@
     enableLuaLoader = true;
     useSystemClipboard = true;
         
-    theme = {
-      enable = true;
-      name = "tokyonight"; #TODO: Sync up with default-style module
-      style = "night";
-    };
+    # theme = {
+    #   enable = true;
+    #   name = "tokyonight"; #TODO: Sync up with default-style module
+    #   style = "night";
+    # };
 
     languages = {
       enableLSP = true;
