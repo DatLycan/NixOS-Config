15914cde5f09ec0068d487fb614c8e69136894a0
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-20 02:31:42 +0100

    Added theming

--- modules/system/default-style/default.nix
+++ modules/system/default-style/default.nix
@@ -15,6 +15,12 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.theme}.yaml";
+    stylix = {
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.theme}.yaml";
+      image = pkgs.fetchurl {
+        url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+        sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+      };
+    };
   };
 }
