c6b96ea43862401178338340cbb6493ae75c4868
Sammie <37188830+DatLycan@users.noreply.github.com>
2025-02-20 04:39:09 +0100

    Update disko.nix

--- tools/installer/disko.nix
+++ tools/installer/disko.nix
@@ -1,60 +1,38 @@
 { lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "2M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      my-disk = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "256M";
+              content = {
+               type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
 
-          swap = {
-            size = "8G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
             };
-          };
 
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
