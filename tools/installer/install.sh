d404a81268890e83b11c42c67efec5a8d28f1a70
DatLycan <37188830+DatLycan@users.noreply.github.com>
2025-02-20 01:16:47 +0100

    Adjusted install script

--- tools/installer/install.sh
+++ tools/installer/install.sh
@@ -2,16 +2,24 @@
 
 set -e  # Exit on error
 
+# Clone GitHub repo into /etc/nixos
+NIXOS_DIR="/etc/nixos"
+GIT_REPO="https://github.com/DatLycan/NixOS.git"
+
+# Ensure the target directory exists and is empty
+rm -rf "$NIXOS_DIR"
+git clone "$GIT_REPO" "$NIXOS_DIR"
+
 # Define variables
-GITHUB_URL="https://github.com/DatLycan/NixOS/blob/installer/tools/installer/disko.nix"
-FILE="disko.nix"
+DISKO_CONFIG_URL="https://github.com/DatLycan/NixOS/raw/installer/tools/installer/disko.nix"
+FILE_NAME="disko.nix"
 
 # Download the file from GitHub
-curl -o "$FILE_NAME" "$GITHUB_URL"
+curl -o "$FILE_NAME" "$DISKO_CONFIG_URL"
 
-# Run disko
-nix run disko --mode destroy,format,mount ./"$FILE_NAME"
+# Run disko using local installation
+disko --mode destroy,format,mount ./"$FILE_NAME"
 
 # Generate config and install NixOS
 nixos-generate-config --root /mnt
-nixos-install
+nixos-install --flake "$NIXOS_DIR"
