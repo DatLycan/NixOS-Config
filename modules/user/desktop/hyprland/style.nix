5412d989e6253b44d65c7d3c42d04d44c0821094
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-03-21 18:22:09 +0100

    Adjusted nord theme

--- modules/user/desktop/hyprland/style.nix
+++ modules/user/desktop/hyprland/style.nix
@@ -13,12 +13,12 @@
     decoration = {
       rounding = 6;
       active_opacity = 1.0;
-      inactive_opacity = 0.85;
+      inactive_opacity = 0.45;
       fullscreen_opacity = 1.0;
 
       blur = {
         enabled = true;
-        size = 6;
+        size = 4;
         passes = 2;
         ignore_opacity = true;
         xray = true;
