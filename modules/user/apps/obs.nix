a2f3cb717caab69d10d0bb6554fc7a88ceee4253
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-03-22 20:50:30 +0100

    Added obs

---
+++ modules/user/apps/obs.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [obs-studio];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+}
