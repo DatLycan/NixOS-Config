162127dfdae5e7b5647622f39643ac54c0ec45d4
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-03-25 22:11:51 +0100

    Added mpv scripts

--- modules/user/apps/mpv.nix
+++ modules/user/apps/mpv.nix
@@ -1,3 +1,22 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [mpv];
+  home.packages = with pkgs; [
+    nodePackages.peerflix # Used for torrent streaming
+  ];
+
+  programs.mpv = {
+    enable = true;
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs; [
+          mpvScripts.builtins.autoload
+          mpvScripts.mpv-webm
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+    );
+  };
 }
