d766d2179477e95936e13fe3f34682215a95fa17
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-03-27 07:14:53 +0100

    Moved mpv to its own dir

---
+++ modules/user/apps/mpv/plugins.nix
@@ -0,0 +1,13 @@
+{
+  home.file = {
+    ".config/mpv/script-opts/detect_image.conf".text = ''
+      command_on_first_image_loaded=apply-profile image; enable-section image; script-message status-line-enable
+      command_on_image_loaded=no-osd set video-pan-x 0; script-message align-border "" -1
+      command_on_non_image_loaded=apply-profile video; disable-section image; script-message status-line-disable; no-osd set video-pan-x 0; no-osd set video-pan-y 0; no-osd set video-zoom 0
+    '';
+
+    ".config/mpv/script-opts/status-line.conf".text = ''
+      enabled=no
+    '';
+  };
+}
