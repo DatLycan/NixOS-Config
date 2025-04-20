2193131f7f0dc41badc17d64c418230faae1ded7
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-04-20 19:36:38 +0200

    Fixed usbip service

--- modules/system/apps/usbip.nix
+++ modules/system/apps/usbip.nix
@@ -23,15 +23,21 @@ in {
       linuxKernel.packages.linux_6_11.usbip
     ];
 
+    boot.kernelModules = [
+      "usbip_core"
+      "usbip_host"
+      "vhci_hcd"
+    ];
+
     systemd.services.usbip-server = lib.mkIf cfg.server.enable {
       description = "USB IP Server";
       after = ["network.target"];
       wantedBy = ["multi-user.target"];
 
       serviceConfig = {
-        Type = "simple";
-        ExecStart = "/run/current-system/sw/bin/usbipd -D";
-        Restart = "always";
+        Type = "forking";
+        ExecStart = "/run/current-system/sw/bin/usbipd";
+
         WorkingDirectory = "/run/current-system/sw/bin/";
         User = "root";
       };
