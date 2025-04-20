c0e173c5f9d7815041a953b6f6fea38d8c5fe0cb
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-04-20 19:01:30 +0200

    Added usbip service

--- hosts/de-laptop/system.nix
+++ hosts/de-laptop/system.nix
@@ -29,6 +29,7 @@ in {
   virtualisation-module.enable = true;
 
   usbip-module.enable = true;
+  usbip-module.server.enable = true;
 
   nvf-module.enable = true;
   environment.sessionVariables.EDITOR = "nvim";
--- modules/system/apps/usbip.nix
+++ modules/system/apps/usbip.nix
@@ -7,12 +7,38 @@
   cfg = config.usbip-module;
 in {
   options.usbip-module = {
-    enable = lib.mkEnableOption "Enable USB IP support";
+    enable =
+      lib.mkEnableOption "Enable USB IP support"
+      // {
+        default = false;
+      };
+
+    server = {
+      enable = lib.mkOption {default = false;};
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      linuxKernel.packages.linux_6_14.usbip
+      linuxKernel.packages.linux_6_11.usbip
     ];
+
+    systemd.services.usbip-server = lib.mkIf cfg.server.enable {
+      description = "USB IP Server";
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "/run/current-system/sw/bin/usbipd -D";
+        Restart = "always";
+        WorkingDirectory = "/run/current-system/sw/bin/";
+        User = "root";
+      };
+    };
+
+    networking.firewall.extraCommands = ''
+      iptables -A nixos-fw -p tcp --source 192.168.0.0/16 --dport 3240 -j nixos-fw-accept
+    '';
   };
 }
