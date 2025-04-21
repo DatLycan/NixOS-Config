4a8f1e8fe9898d38897c8e6c7b9cde7fdbe319d8
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-04-21 05:26:51 +0200

    Changed idle timout

--- modules/user/desktop/hyprland/plugins/hypridle.nix
+++ modules/user/desktop/hyprland/plugins/hypridle.nix
@@ -14,11 +14,11 @@
 
       listener = [
         {
-          timeout = 180;
+          timeout = 300;
           on-timeout = "hyprlock";
         }
         {
-          timeout = 300;
+          timeout = 600;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
