6f64271222e4e81f8527ebcd8c4c7741de51a907
DatLycan <37188830+DatLycan@user.noreply.github.com>
2025-04-22 19:21:43 +0200

    Changed mouse dpi

--- hosts/de-laptop/system.nix
+++ hosts/de-laptop/system.nix
@@ -29,7 +29,6 @@ in {
   virtualisation-module.enable = true;
 
   usbip-module.enable = true;
-  usbip-module.server.enable = true;
 
   nvf-module.enable = true;
   environment.sessionVariables.EDITOR = "nvim";
